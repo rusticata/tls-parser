c844c0d04b8ad38eda476619e21c50e170ae7891
Pierre Chifflier <chifflier@wzdftpd.net>
2016-09-19 21:06:29 +0200

    Split x509 (wanna-be) parser code to a dedicated project

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,6 @@ pub mod common;
 
 extern crate der_parser;
 
-pub use x509::*;
-pub mod x509;
-
 pub use tls_alert::*;
 #[macro_use]
 pub mod tls_alert;
@@ -23,14 +20,3 @@ pub use tls_extensions::*;
 pub mod tls_extensions;
 pub use tls::*;
 pub mod tls;
-
-//mod common;
-
-#[cfg(test)]
-mod tests {
-    //use x509::*;
-
-    #[test]
-    fn it_works() {
-    }
-}
