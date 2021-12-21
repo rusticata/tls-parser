6e64f82860e40559abdf2b6224bb466e5f6f9dcf
Pierre Chifflier <chifflier@wzdftpd.net>
2021-12-21 11:51:45 +0100

    Implement TryFrom u16, TlsCipherSuiteID and &str for TlsCipherSuite

--- src/tls_ciphers.rs
+++ src/tls_ciphers.rs
@@ -8,7 +8,13 @@
 #![allow(non_camel_case_types)]
 #![allow(clippy::unreadable_literal)]
 
+use core::convert::TryFrom;
 use enum_primitive::{enum_from_primitive, enum_from_primitive_impl, enum_from_primitive_impl_ty};
+
+use crate::TlsCipherSuiteID;
+
+#[derive(Debug)]
+pub struct CipherSuiteNotFound(());
 
 enum_from_primitive! {
 /// Key exchange methods
@@ -113,18 +119,48 @@ pub struct TlsCipherSuite {
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
 impl TlsCipherSuite {
+    /// Attempt to get reference on `TlsCipherSuite` identified by `id`.
     pub fn from_id(id: u16) -> Option<&'static TlsCipherSuite> {
         CIPHERS.get(&id)
     }
 
+    /// Attempt to get reference on `TlsCipherSuite` identified by `name`.
     pub fn from_name<'a>(name: &'a str) -> Option<&'static TlsCipherSuite> {
         CIPHERS.values().find(|&v| v.name == name)
+    }
+}
+
+impl TryFrom<u16> for &'static TlsCipherSuite {
+    type Error = CipherSuiteNotFound;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        CIPHERS.get(&value).ok_or(CipherSuiteNotFound(()))
+    }
+}
+
+impl TryFrom<TlsCipherSuiteID> for &'static TlsCipherSuite {
+    type Error = CipherSuiteNotFound;
+
+    fn try_from(value: TlsCipherSuiteID) -> Result<Self, Self::Error> {
+        CIPHERS.get(&value.0).ok_or(CipherSuiteNotFound(()))
+    }
+}
+
+impl<'a> TryFrom<&'a str> for &'static TlsCipherSuite {
+    type Error = CipherSuiteNotFound;
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        CIPHERS
+            .values()
+            .find(|&v| v.name == value)
+            .ok_or(CipherSuiteNotFound(()))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::tls_ciphers::{TlsCipherKx, TlsCipherSuite, CIPHERS};
+    use core::convert::TryFrom;
 
     #[test]
     fn test_cipher_count() {
@@ -134,13 +170,13 @@ mod tests {
 
     #[test]
     fn test_cipher_from_id() {
-        let cipher = TlsCipherSuite::from_id(0xc025).expect("could not get cipher");
+        let cipher = <&TlsCipherSuite>::try_from(0xc025).expect("could not get cipher");
         println!("Found cipher: {:?}", cipher);
     }
 
     #[test]
     fn test_cipher_from_name() {
-        let cipher = TlsCipherSuite::from_name("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384")
+        let cipher = <&TlsCipherSuite>::try_from("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384")
             .expect("could not get cipher");
         println!("Found cipher: {:?}", cipher);
     }
